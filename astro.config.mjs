41434d9b2ef39a759a87d1b7ebe0d7eb4c478e03
Quinn <nyankina00@proton.me>
2024-12-05 11:02:59 +0200

    Work out bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-	site: 'https://nyankina.github.io'
+	site: 'https://nyankina.github.io',
+	base: 'website'
 })
